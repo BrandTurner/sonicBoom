453780116607a41eafe5b170e681fbe6853205f6
Brandon Turner <bturner@gmail.com>
2016-11-06 19:37:52 -0800

    Receiving access token, need to make api request using params and scopes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,15 @@
 var React = require('react');
 var MainContainer = require('./MainContainer');
-import { Grid, Image, Segment } from 'semantic-ui-react'
+import { Grid, Image, Segment } from 'semantic-ui-react';
+import { GoogleLogin } from 'react-google-login-component';
 
 var Home = React.createClass({
+    handleGoogleResponse: function(googleUser) {
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log({accessToken: id_token});
+        console.log({User: googleUser});
+        //anything else you want to do(save to localStorage)...
+    },
     render: function() {
         return (
             <MainContainer>
@@ -12,6 +19,12 @@ var Home = React.createClass({
                 <div id="login-container">This application requires access to your YouTube account.
                     Please <a href="#" id="login-link">authorize</a> to continue.
                 </div>
+
+                <GoogleLogin socialId="520012681222-mnejve8atjj13r6rr0ellm6s1eltpip9.apps.googleusercontent.com"
+                    class="google-login"
+                    scope="profile"
+                    responseHandler={this.handleGoogleResponse}
+                    buttonText="Login With Google"/>
 
                 <Grid stackable columns={2}>
                     <Grid.Column>
